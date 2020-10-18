cef70b8016357df7152b55caeeba1825b2f9f82f
Denis Filipovich <philippovichdenis@gmail.com>
2020-10-18 22:14:12 +0300

    add some fixs

---
+++ optimized/js/common.js
@@ -0,0 +1,69 @@
+function init() {
+  var container = document.querySelector(".container");
+  container.classList.add("active");
+  setTimeout(function() {
+      var screen1 = document.querySelector('.screen-1');
+      var screen2 = document.querySelector('.screen-2');
+      screen1.classList.add('disactive');
+      screen2.classList.add('active');
+  },1000)
+}
+
+function hideTape() {
+  var container = document.querySelector(".container");
+  var cursor = document.querySelector(".screen-2__cursor");
+
+  cursor.classList.add('hidden');
+  container.classList.add("fact-show");
+
+
+  
+}
+
+function restoreTape() {
+  console.log('Clicked')
+  var container = document.querySelector(".container");
+  var facts = document.querySelectorAll(".fact");
+
+  container.classList.remove("fact-show");
+
+  for(var i = 0; i <facts.length; i++) {
+    facts[i].classList.remove("slide-in-elliptic-top-fwd");
+  }
+}
+
+function showFact(target) {
+  var tgt = document.querySelector("." + target);
+
+  tgt.classList.add("slide-in-elliptic-top-fwd");
+}
+
+var frames = document.querySelectorAll(".frame");
+var backToTapeBtns = document.querySelectorAll("[data-js='back-to-tape']");
+
+window.addEventListener("load", function () {
+  init();
+});
+
+for(var i = 0; i < frames.length; i++) {
+  frames[i].addEventListener("click", function (el) {
+    var frameToshow = el.target.getAttribute("data-target");
+    hideTape();
+      showFact(frameToshow);
+  });
+
+  // frames[i].addEventListener("touchend", function (el) {
+  //   var frameToshow = el.target.getAttribute("data-target");
+  //   hideTape();
+  //     showFact(frameToshow);
+  // });
+}
+
+for(var i = 0; i < backToTapeBtns.length; i++) {
+  backToTapeBtns[i].addEventListener("click", function () {
+    restoreTape();
+  });
+  // backToTapeBtns[i].addEventListener("touchend", function () {
+  //   restoreTape();
+  // });
+}
